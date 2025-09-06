f8d4bc2e7d338218be7185c3aa4a4b3e71c8fa5e
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-09-06 17:15:01 +0700

    make Product model and migrate it

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+
+    # required columns
+    name = models.CharField(max_length=255) 
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField() # sebagai tempat menyimpan link gambar dari produk
+    category = models.CharField(max_length=50) 
+    is_featured = models.BooleanField(default=False) # status unggulan item
+    # adding more columns I insisted
+    stock = models.PositiveIntegerField(default=0) # tempat menyimpan banyaknya stock dari produk
+    created_at = models.DateTimeField(auto_now_add=True) # tempat menyimpan tanggal dibuatnya sebuah produk
+    updated_at = models.DateTimeField(auto_now=True) # tempat menyimpan tanggal setiap produk mengalami perubahan
+
+    # sebagai representasi string dari setiap object Product
+    def __str__(self):
+        return self.name
