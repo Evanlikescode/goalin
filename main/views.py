cf4d931f549ff79f663c1be4b58caeb0226dbbd8
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-09-06 17:16:52 +0700

    configure the landing page view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def landing_page(request):
+
+    data = {
+        'app_name': 'goalin',
+        'creator_name': 'Evan Haryo Widodo',
+        'creator_class': 'PBP A'
+    }
+
+    return render(request, "landing_page.html", data)
