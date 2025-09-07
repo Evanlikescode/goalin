7cb0ddbf2c313185e3f5eba74703c45390d01e8e
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-09-07 14:47:11 +0700

    make choices on category columns

--- main/models.py
+++ main/models.py
@@ -4,12 +4,18 @@ from django.db import models
 
 class Product(models.Model):
 
+    # category choices
+    category_choices = [
+        ('outdoor', 'Outdoor Items'),
+        ('indoor', 'Indoor Items')
+    ]
+
     # required columns
     name = models.CharField(max_length=255) 
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField() # sebagai tempat menyimpan link gambar dari produk
-    category = models.CharField(max_length=50) 
+    category = models.CharField(max_length=50, choices=category_choices) 
     is_featured = models.BooleanField(default=False) # status unggulan item
     # adding more columns I insisted
     stock = models.PositiveIntegerField(default=0) # tempat menyimpan banyaknya stock dari produk
