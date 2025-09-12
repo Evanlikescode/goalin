5564578987d6490133848fe4c98a6b9c9f5242ea
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-09-13 01:01:17 +0700

    add some routings and add more views

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from main.views import landing_page
+from main.views import landing_page, create_product, show_product, show_json
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', landing_page, name='landing_page')
+    path('', landing_page, name='landing_page'),
+    path('create-product/', create_product, name='create_product'),
+    path('product/<str:id>/', show_product, name='show_product_id'),
+    path('product/', show_product, name='show_product'),
+    path('json/', show_json, name='show_json'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,13 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.models import Product
+from main.forms import ProductForm
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 
 def landing_page(request):
 
+    product_list = Product.objects.all()
+
     data = {
         'app_name': 'goalin',
         'creator_name': 'Evan Haryo Widodo',
-        'creator_class': 'PBP A'
+        'creator_class': 'PBP A',
+        'product_list' : product_list
     }
 
     return render(request, "landing_page.html", data)
+
+
+def create_product(request):
+
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:landing_page')
+
+    data = {'form': form}
+    return render(request, "create_product.html", data)
+
+def show_product(request, id=None):
+    if id is None:
+        product_list = Product.objects.all()
+        data = {
+            'product_list' : product_list
+        }
+        return render(request, "show_product.html", data)
+    
+    product = get_object_or_404(Product, pk=id)
+
+    data = {
+        'data': product
+    }
+
+    return render(request, "more_product", data)
+
+
+def show_json(request):
+    news_list = Product.objects.all()
+    json_data = serializers.serialize("json", news_list)
+    return HttpResponse(json_data, content_type="application/json")
