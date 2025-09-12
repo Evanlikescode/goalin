3d1a3fc5f95e5579bd10e0d857d85fe042e31763
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-09-13 01:02:19 +0700

    implement form creation

---
+++ main/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from main.models import Product
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "price",
+            "description",
+            "thumbnail",
+            "category",
+            "is_featured",
+            "stock"
+        ]
